a92238f451856df9051b35ad6dd948468d80771f
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-08-22 00:41:09 +0600

    Cleaner Code

--- app/controller/main-ctrl.js
+++ app/controller/main-ctrl.js
@@ -2,7 +2,7 @@
     angular.module('attendance')
     .controller('mainCtrl', function($scope){
         $scope.theme = 'dark';
-        $scope.logoSrc = "app/images/logo/logo.jpg";
+        $scope.logoSrc = "app/images/logo/logo.png";
         $scope.$on('themeChanged', function($event, message){
             $scope.theme = message;
         });
