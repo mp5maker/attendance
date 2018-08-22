ebf37a49ee7fcf08455ec98905524841f1637767
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-08-23 00:49:08 +0600

    Fixed Slight Bug Excel

--- assets/main.js
+++ assets/main.js
@@ -624,20 +624,6 @@
         }
     });
 })();
-(function(){    
-    angular.module('attendance')
-    .filter('numberTranslate', function($translate){
-        return function(number, language){
-            language_key = language;
-            var languageTable = $translate.getTranslationTable(language_key);
-            var create_number = "";
-            for(var i = 0; i < number.length; i++){
-                create_number += languageTable[number[i]];
-            }
-            return create_number;
-        }
-    });
-})();
 (function(){
     "use strict";
     
@@ -656,4 +642,19 @@
                 }
             };
         })
+})();
+
+(function(){    
+    angular.module('attendance')
+    .filter('numberTranslate', function($translate){
+        return function(number, language){
+            language_key = language;
+            var languageTable = $translate.getTranslationTable(language_key);
+            var create_number = "";
+            for(var i = 0; i < number.length; i++){
+                create_number += languageTable[number[i]];
+            }
+            return create_number;
+        }
+    });
 })();
