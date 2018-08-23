5813c288639c62c68c13ba025c936adb9046128e
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-08-23 11:47:50 +0600

    Fixed Issues

--- app/filters/number-translate.js
+++ app/filters/number-translate.js
@@ -1,9 +1,15 @@
-(function(){    
+(function(){   
+    "use strict";
+
     angular.module('attendance')
     .filter('numberTranslate', function($translate){
-        return function(number, language){
-            language_key = language;
-            var languageTable = $translate.getTranslationTable(language_key);
+        return function(number, language_key){
+            var languageTable;
+            if(language_key){
+                languageTable = $translate.getTranslationTable(language_key);
+            }else{
+                languageTable = $translate.getTranslationTable('en');
+            }
             var create_number = "";
             for(var i = 0; i < number.length; i++){
                 create_number += languageTable[number[i]];
